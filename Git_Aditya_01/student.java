0fbbfe81fc1b20c7722980e1ea871efa2cf80715
Aditya Patel <115413641+aditya-ptl-27@users.noreply.github.com>
2023-05-30 16:02:34 +0530

    Add files via upload

---
+++ Git_Aditya_01/student.java
@@ -0,0 +1,8 @@
+public class Student1 {
+    public static void main(String[] args) {
+        Student1 student = new Student1();
+        student.getData();
+        System.out.println("Student Details:");
+        student.display();
+    }
+}
