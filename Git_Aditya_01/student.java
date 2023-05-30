c90c6b3511b1698686a8f97bccbc880802213016
Aditya Patel <115413641+aditya-ptl-27@users.noreply.github.com>
2023-05-30 16:12:56 +0530

    Delete Git_Aditya_01 directory

--- Git_Aditya_01/student.java
+++
@@ -1,8 +0,0 @@
-public class Student1 {
-    public static void main(String[] args) {
-        Student1 student = new Student1();
-        student.getData();
-        System.out.println("Student Details:");
-        student.display();
-    }
-}
